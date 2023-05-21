9f8ea8f92384d3689e9675c775e573b090f54a16
Anastasija <127342966+ShaniThePug@users.noreply.github.com>
2023-05-21 16:45:39 +0300

    Add files via upload

---
+++ PP_2_1_2_Spring_HelloWorld/src/main/java/Cat.java
@@ -0,0 +1,13 @@
+public class Cat { //POJO-класс Cat
+    private String catMeow;
+
+    public String getCatSound() {
+        return catMeow;
+    }
+
+    public void setCatMeow(String catSound) {
+        this.catMeow = catSound;
+    }
+}
+
+
